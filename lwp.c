11ebf3657c74fa85569f0e3ba0c76b017cbea563
ljbarker <loganjb21@outlook.com>
2024-10-11 13:06:53 -0700

    finish and document rrAdmit

--- lwp.c
+++ lwp.c
@@ -2,6 +2,11 @@
 #include "fp.h"
 #include <stdlib.h>
 
+/*
+schedule_one is the next thread in the list
+schedule_two is the previous thread in the list
+*/
+
 thread head = NULL;
 thread tail = NULL;
 unsigned long qLen = 0;
@@ -16,21 +21,34 @@ void rrShtudown(){
 
 void rrAdmit(thread new){
     if(head == NULL){
+        /*
+        If there are no threads, initialize list
+        with the new thread, assigning next and prev
+        pointers to NULL and our number of ready processes
+        to 1. Set head and tail to the new thread
+        */
         head = new;
         tail = new;
         new->sched_one = NULL;
-        new->sched_two = new;
-        new
+        new->sched_two = NULL;
         qLen = 1;
     }
     else{
+        /*
+        If there are scheduled threads, set the
+        next pointer of the tail thread
+        to the new thread, set the next pointer
+        of the new thread to NULL, set the previous
+        pointer of the new thread to the current tail,
+        and update the tail pointer to the new thread.
+        Increment the number of ready processes 
+        */
         tail->sched_one = new;
-        new->sched_one = NULL
+        new->sched_one = NULL;
         new->sched_two = tail;
         tail = new;
         qLen += 1;
     }
-    
 }
 
 void rrRemove(thread victim){

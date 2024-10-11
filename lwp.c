ecb4e83e84860c44c2b29a225630a05914b4ab6c
RandomCyberCoder <emmanueldelgado74@gmail.com>
2024-10-11 12:25:13 -0700

    base

---
+++ lwp.c
@@ -0,0 +1,26 @@
+Header head = NULL
+Header tail = NULL
+void rrInit(){
+    //do we want to do anything here
+}
+
+void rrShtudown(){
+    //do we want to do anything here?????
+}
+
+void rrAdmit(thread new){
+    //add this thread to the end of the list
+}
+
+void rrRemove(thread victim){
+    //find the thread we want to remove
+}
+
+thread rrNext(){
+    //return the thread at the start of the list and append it to the end;
+    //check for case of only 1 thread?
+}
+
+int rrqlen(){
+    //create a var for this???
+}

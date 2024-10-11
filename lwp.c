6929ebc3af11f12b800cb40e38d1723260b17bbb
RandomCyberCoder <emmanueldelgado74@gmail.com>
2024-10-11 12:50:23 -0700

    rrAdmit done

--- lwp.c
+++ lwp.c
@@ -1,5 +1,9 @@
-Header head = NULL
-Header tail = NULL
+#include "lwp.h"
+
+thread head = NULL
+thread tail = NULL
+unsigned long qLen = 0;
+
 void rrInit(){
     //do we want to do anything here
 }
@@ -9,11 +13,39 @@ void rrShtudown(){
 }
 
 void rrAdmit(thread new){
-    //add this thread to the end of the list
+    if(head == NULL){
+        head = new;
+        tail = new;
+        new->sched_one = NULL;
+        new->sched_two = new;
+        new
+        qLen = 1;
+    }
+    else{
+        tail->sched_one = new;
+        new->sched_one = NULL
+        new->sched_two = tail;
+        tail = new;
+        qLen += 1;
+    }
+    
 }
 
 void rrRemove(thread victim){
     //find the thread we want to remove
+    if(victim == head){
+        head = head->sched_one;
+        qLen -= 1;
+        return;
+    }
+
+    thread curThread = head;
+    while(curThread != victim){
+
+    }
+
+    qLen -= 1;
+
 }
 
 thread rrNext(){
@@ -22,5 +54,5 @@ thread rrNext(){
 }
 
 int rrqlen(){
-    //create a var for this???
+    return qLen;
 }
